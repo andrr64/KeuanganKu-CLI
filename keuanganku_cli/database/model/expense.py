c0a88de4fb28eb5b4b3b48a4c04ff2836e5533ba
andrr64 <derzandrr03@gmail.com>
2024-01-29 02:00:57 +0700

    rename file

--- keuanganku_cli/database/model/expense.py
+++ keuanganku_cli/database/model/expense.py
@@ -2,13 +2,13 @@ from dataclasses import dataclass
 from datetime import datetime
 
 @dataclass
-class ModelPengeluaran:
+class ModelExpense:
     id: int
-    judul: str
-    waktu: datetime
-    jumlah: float
-    id_kategori: int
-    rating: int 
+    title: str
+    time: datetime
+    amount: float
+    category_id: int
+    rate: int 
 
     def __post_init__(self):
         pass
