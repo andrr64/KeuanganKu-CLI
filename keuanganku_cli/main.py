b975020633f8955f89e5fa63285ccad967278a02
andrr64 <derzandrr03@gmail.com>
2024-01-28 23:33:42 +0700

    Implements database

--- keuanganku_cli/main.py
+++ keuanganku_cli/main.py
@@ -1,4 +1,12 @@
 from UI.routes.homepage.home import *
+from database.db import KDatabase
 
 if __name__ == "__main__":
-    ui_homepage()
+
+    # Check is database exist or not
+    if not KDatabase.isExist():
+        KDatabase().initDatabase()
+
+    db = KDatabase()
+    ui_homepage(db)
+    db.closeConnection()
