e5e86fc6cb0dbccad7ce07cb19ab7b4553a3c13a
Andreas <derzandrr03@gmail.com>
2024-02-01 09:17:09 +0700

    update data model structure

--- keuanganku_cli/database/model/expense.py
+++ keuanganku_cli/database/model/expense.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from database.model.expense_category import ModelExpenseCategory
 
 @dataclass
 class ModelExpense:
@@ -7,7 +8,8 @@ class ModelExpense:
     title: str
     time: datetime
     amount: float
-    category_id: int
+    category_id : int
+    category : ModelExpenseCategory
     rate: int 
 
     def __post_init__(self):
@@ -15,44 +17,25 @@ class ModelExpense:
     
     def toListForInsert(self):
         '''The return must ordered same like 'expense' column order '''
-        return [self.title, self.timeToStringFormat, self.amount, self.category_id, self.rate]
+        return [self.title, self.timeToStringFormat, self.amount, self.category.id, self.rate]
 
     @property
     def timeToStringFormat(self) -> str:
         return datetime.strftime(self.time, "%d/%m/%y %H:%S")
-
+    
     @staticmethod
-    def toJson(expense):
-        return {
-            'id': expense.id,
-            'title': expense.title,
-            'time': expense.time.strftime('%Y-%m-%d %H:%M:%S'),  # Format sesuai kebutuhan Anda
-            'amount': expense.amount,
-            'category_id': expense.category_id,
-            'rate': expense.rate
-        }
-
-    @staticmethod
-    def fromTuple(tupleData):
+    def fromTuple(tupleData, expenseCategory : ModelExpenseCategory):
         return ModelExpense(
             id=tupleData[0],
             title=tupleData[1],
             time=datetime.strptime(tupleData[2], "%d/%m/%y %H:%M" ),
             amount=tupleData[3],
-            category_id=tupleData[4],
+            category= expenseCategory,
+            category_id=expenseCategory.id,
             rate=tupleData[5]
         )
 
-    @staticmethod
-    def fromJson(json_data):
-        return ModelExpense(
-            id=json_data['id'],
-            title=json_data['title'],
-            time=datetime.strptime(json_data['time'], '%Y-%m-%d %H:%M:%S'),  # Sesuaikan dengan format yang digunakan dalam toJson
-            amount=json_data['amount'],
-            category_id=json_data['category_id'],
-        )
-            
     def __str__(self) -> str:
         amount = f'{self.amount:,.0f}'
-        return f"{self.title:<10} | {amount:<13}"
+        categoryTitle = f'{self.category.title:<20}'
+        return f"{self.title:<10} | {amount:<13} | {categoryTitle[:20]}"
