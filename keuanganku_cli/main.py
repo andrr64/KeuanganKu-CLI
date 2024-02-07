2144398ba409df284f1bf6dc0eb15c77b6204a2d
andrr64 <derzandrr03@gmail.com>
2024-02-08 01:54:33 +0700

    add CHECK steps

--- keuanganku_cli/main.py
+++ keuanganku_cli/main.py
@@ -1,13 +1,34 @@
 from UI.routes.homepage.home import *
 from database.db import KDatabase
 
+def __CHECK_matplotlibModule():
+    try:
+        import matplotlib
+        del matplotlib
+    except:
+        errorMsg = 'Matplotlib is not found, install it using pip command "pip install matplotlib"'
+        raise Exception(errorMsg)
+    
+def __CHECK_database():
+    try:
+        if not KDatabase.isExist():
+            KDatabase().initDatabase()
+    except:
+        errorMsg = "Database error, something wrong..."
+        raise Exception(errorMsg)
+    
+def CHECK_isEverythingOkay():
+    try:
+        __CHECK_database()
+        __CHECK_matplotlibModule()
+        return True
+    except Exception as e:
+        kprintInfo(e)
+        return False
+    
 if __name__ == "__main__":
-
-    # Check is database exist or not
-    if not KDatabase.isExist():
-        KDatabase().initDatabase()
-
-    db = KDatabase()
-    ui_homepage(db)
-    if KDatabase.isExist():
-        db.closeConnection()
+    if CHECK_isEverythingOkay() is True:
+        db = KDatabase()
+        UI_homepage(db)
+        if KDatabase.isExist():
+            db.closeConnection()
