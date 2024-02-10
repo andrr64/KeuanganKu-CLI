2829ad7fbe8dea24a58b308ecf8182bd51e30835
andrr64 <derzandrr03@gmail.com>
2024-02-10 20:36:41 +0700

    Add 'detailExpenseCategory' interface

--- keuanganku_cli/UI/routes/expense_data/routes/category.py
+++ keuanganku_cli/UI/routes/expense_data/routes/category.py
@@ -5,25 +5,42 @@ from UI.utility.clearscreen import clrscreen
 from UI.utility.ui_print import kprint, kline, kprintInfo
 from UI.user_input.input import getAny
 
-def UI_homepageCategory(db : KDatabase):
-    try:
-        categoryList = SQLExpenseCategory().readAll(db.connection)
+from UI.routes.expense_data.routes.routes.detail_category import UI_expenseCategoryDetail
+global_categoryList = []
+global_categoryListLen = 0
 
+def VAR_updateExpenseCategoryList(db : KDatabase):
+    global global_categoryList
+    global global_categoryListLen
+    global_categoryList = SQLExpenseCategory().readAll(db.connection)
+    global_categoryListLen = len(global_categoryList)
+
+def UI_homepageCategory(db : KDatabase):
+    global global_categoryList
+    global global_categoryListLen
+    try:
+        VAR_updateExpenseCategoryList(db)
         while True:
             clrscreen()
             kline()
             kprint("Expense Category")
             kline()
             kprint(f"{'No':<4} | Title")
-            for index, category in enumerate(categoryList):
+            for index, category in enumerate(global_categoryList):
                 kprint(f"{str(index+1):<4} | {category}")
             kline()
             kprint("i : Insert\t") 
             kprint("e : Back\t| h : Help\t| r : Refresh") 
             kline()
             userInput = str.lower(getAny('Command'))
-            if userInput == 'e':
-                break
+            try:
+                choosedIndex = int(userInput)-1
+                if choosedIndex >= 0 and choosedIndex < global_categoryListLen:
+                    if UI_expenseCategoryDetail(db, global_categoryList[choosedIndex]):
+                        VAR_updateExpenseCategoryList(db)
+            except:
+                if userInput == 'e':
+                    break
     except Exception as e:
         clrscreen()
         kprintInfo(e)
---
+++ keuanganku_cli/UI/routes/expense_data/routes/routes/detail_category.py
@@ -0,0 +1,36 @@
+from database.model.model_expense_category import ModelExpenseCategory
+from database.db import KDatabase
+from database.helper.sql_expense_category import SQLExpenseCategory
+
+from UI.utility.clearscreen import clrscreen
+from UI.user_input.input import getAny
+from UI.utility.ui_print import kprint, kprintInfo, kline
+
+def DB_deleteExpenseCategory(db : KDatabase, data : ModelExpenseCategory) -> bool:
+    clrscreen()
+    if SQLExpenseCategory().delete(db.connection, data):
+        kprintInfo('Operation success ^_^')
+        return True
+    kprintInfo('Something wrong...')
+    return False
+
+def UI_expenseCategoryDetail(db : KDatabase, data : ModelExpenseCategory) -> any:
+    while True:
+        clrscreen()
+        kline()
+        kprint("Detail")
+        kline()
+        kprint(f"Title\t: {data.title}")
+        kline()
+        kprint("Command")
+        kprint("e: Back\t| d: Delete\t| u: Update")
+        kline()
+        userInput = str.lower(getAny('Command'))
+        if userInput == 'd':
+            clrscreen()
+            userInput = str.lower(getAny('Are you sure? [Y/n]'))
+            if userInput == 'y':
+                DB_deleteExpenseCategory(db, data)
+                return True
+        elif userInput == 'e':
+            return None
